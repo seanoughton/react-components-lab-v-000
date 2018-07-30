b081ffc0db7d98d95002a7d8da76109457220deb
Sean Oughton <seanoughton@gmail.com>
2018-07-30 23:31:54 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -14,12 +14,14 @@ export class OlderCoaster extends Component {
   // this function should return the JSX we want the DOM to render
    render() {
      return (
+       <div class = "oldercoaster">
        <p>Two grannies having the time of their life!</p>
        <p>Passengers:</p>
         <ul>
           <li>Agnes</li>
           <li>Muriel</li>
         </ul>
+       </div>
      )
    }
   //
