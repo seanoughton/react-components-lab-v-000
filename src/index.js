99e273023f7abbe8a11feaeacdc39ce4a199559b
Sean Oughton <seanoughton@gmail.com>
2018-07-30 23:46:25 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ export class InFrontOfYou extends Component {
 
 export class ButcherShop extends Component {
   render() {
+    const products = BUTCHER_PRODUCTS.map((p, idx) => (<li key={idx}>{p}</li>))
     return (
       <div class="butcher-shop">
         <p>Hello! We have the following products for sale today:</p>
