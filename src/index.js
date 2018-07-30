7c539de0cfd44d9ba3226891bfcf0f525a9db12d
Sean Oughton <seanoughton@gmail.com>
2018-07-30 23:44:44 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -59,7 +59,9 @@ export class App extends Component {
   render() {
     return (
       <div id="app">
-
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
       </div>
     )
   }
